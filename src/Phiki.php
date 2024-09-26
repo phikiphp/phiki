1a152cc6c58da67c46a289e165e47d9e82f367a6
Ryan Chandler <ryangjchandler@gmail.com>
2024-09-26 12:57:31 +0100

    wip

--- src/Phiki.php
+++ src/Phiki.php
@@ -2,9 +2,36 @@
 
 namespace Phiki;
 
+use Phiki\Contracts\GrammarRepositoryInterface;
+use Phiki\Contracts\ThemeRepositoryInterface;
+
+/**
+ * @method static string codeToHtml(string $code, string $grammar, string $theme): string
+ */
 class Phiki
 {
     public function __construct(
-        protected GrammarRepository $grammarRepository = new GrammarRepository,
+        protected GrammarRepositoryInterface $grammarRepository = new GrammarRepository,
+        protected ThemeRepositoryInterface $themeRepository = new ThemeRepository,
     ) {}
+
+    public function codeToHtml(string $code, string $grammar, string $theme): string
+    {
+        $grammar = $this->grammarRepository->get($grammar);
+        $theme = $this->themeRepository->get($theme);
+
+        $tokenizer = new Tokenizer($grammar);
+        $tokens = $tokenizer->tokenize($code);
+
+        $styles = new ThemeStyles($theme);
+        $highlighter = new Highlighter($styles);
+        $htmlGenerator = new HtmlGenerator($styles);
+
+        return $htmlGenerator->generate($highlighter->highlight($tokens));
+    }
+
+    public static function default(): static
+    {
+        return new static(new GrammarRepository, new ThemeRepository);
+    }
 }
--- tests/Unit/PhikiTest.php
+++ tests/Unit/PhikiTest.php
@@ -6,4 +6,12 @@ describe('Phiki', function () {
     it('can be constructed', function () {
         expect(new Phiki())->toBeInstanceOf(Phiki::class);
     });
+
+    it('can generate html from code', function () {
+        expect(Phiki::default()->codeToHtml(<<<'PHP'
+        function add(int|float $a, int|float $b): int|float {
+            return $a + $b;
+        }
+        PHP, 'php', 'github-dark'))->toBeString();
+    });
 });
