4d2dcac4d0f39442e5309fca29e75923f0264b02
Ryan Chandler <ryangjchandler@gmail.com>
2024-10-10 00:27:56 +0100

    chore: remove dump

--- src/Grammar/MatchPattern.php
+++ src/Grammar/MatchPattern.php
@@ -20,7 +20,6 @@ class MatchPattern extends Pattern implements ContainsCapturesInterface
 
     public function tryMatch(Tokenizer $tokenizer, string $lineText, int $linePosition, ?int $cannotExceed = null): MatchedPattern|false
     {
-        // dump($this->match->get($tokenizer->allowA(), $tokenizer->allowG()));
         if (preg_match('/'.$this->match->get($tokenizer->allowA(), $tokenizer->allowG()).'/u', $lineText, $matches, PREG_OFFSET_CAPTURE, $linePosition) !== 1) {
             return false;
         }
