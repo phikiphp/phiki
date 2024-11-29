4d9088565df2b26ce3a448c3c0dc2c772dd97dca
Bogdan Condorachi <bogdan.condorachi@gmail.com>
2024-11-29 19:05:28 +0200

    refactor: simplify html generator

--- src/Generators/HtmlGenerator.php
+++ src/Generators/HtmlGenerator.php
@@ -20,129 +20,123 @@ class HtmlGenerator implements OutputGeneratorInterface
 
     public function generate(array $tokens): string
     {
-        $html = [];
-        $defaultTheme = Arr::first($this->themes);
-        $defaultThemeId = Arr::firstKey($this->themes);
+        return $this->withWrapper ? $this->buildWrapper($tokens) : $this->buildPre($tokens);
+    }
 
-        if ($this->withWrapper) {
-            $wrapperStyles = [
-                $defaultTheme->base()->toStyleString(),
-            ];
+    private function buildWrapper($tokens): string
+    {
+        $wrapperStyles = [$this->getDefaultTheme()->base()->toStyleString()];
 
-            foreach ($this->themes as $id => $theme) {
-                if ($id === $defaultThemeId) {
-                    continue;
-                }
-
+        foreach ($this->themes as $id => $theme) {
+            if ($id !== $this->getDefaultThemeId()) {
                 $wrapperStyles[] = $theme->base()->toCssVarString($id);
             }
-
-            $html[] = sprintf(
-                '<div class="phiki-wrapper" style="%s"%s>',
-                implode(';', $wrapperStyles),
-                $this->grammarName ? sprintf(' data-language="%s"', $this->grammarName) : '',
-            );
         }
 
-        $preClasses = ['phiki', $this->grammarName ? 'language-'.$this->grammarName : null, $defaultTheme->name];
+        return sprintf(
+            '<div class="phiki-wrapper"%s style="%s">%s</div>',
+            $this->grammarName ? " data-language=\"$this->grammarName\"" : null,
+            implode(';', $wrapperStyles),
+            $this->buildPre($tokens)
+        );
+    }
 
-        if (count($this->themes) > 1) {
-            $preClasses[] = 'phiki-themes';
+    private function buildPre($tokens): string
+    {
+        $preClasses = array_filter([
+            'phiki',
+            $this->grammarName ? "language-$this->grammarName" : null,
+            $this->getDefaultTheme()->name,
+            count($this->themes) > 1 ? 'phiki-themes' : null,
+        ]);
 
-            foreach ($this->themes as $theme) {
-                if ($theme === $defaultTheme) {
-                    continue;
-                }
-
+        foreach ($this->themes as $theme) {
+            if ($theme !== $this->getDefaultTheme()) {
                 $preClasses[] = $theme->name;
             }
         }
 
-        $preStyles = [
-            $defaultTheme->base()->toStyleString(),
-        ];
+        $preStyles = [$this->getDefaultTheme()->base()->toStyleString()];
 
         foreach ($this->themes as $id => $theme) {
-            if ($id === $defaultThemeId) {
-                continue;
+            if ($id !== $this->getDefaultThemeId()) {
+                $preStyles[] = $theme->base()->toCssVarString($id);
             }
-
-            $preStyles[] = $theme->base()->toCssVarString($id);
         }
 
-        $html[] = sprintf(
-            '<pre class="%s" style="%s"%s>',
-            implode(' ', array_filter($preClasses)),
+        return sprintf(
+            '<pre class="%s"%s style="%s">%s</pre>',
+            implode(' ', $preClasses),
+            $this->grammarName ? " data-language=\"$this->grammarName\"" : null,
             implode(';', $preStyles),
-            $this->grammarName ? sprintf(' data-language="%s"', $this->grammarName) : '',
+            $this->buildCode($tokens)
         );
+    }
 
-        $html[] = '<code>';
-
+    private function buildCode(array $tokens): string
+    {
         foreach ($tokens as $i => $line) {
-            $html[] = sprintf(
-                '<span class="line" data-line="%d">',
-                $i + 1,
-            );
+            $output[] = $this->buildLine($line, $i);
+        }
 
-            if ($this->withGutter) {
-                $lineNumberStyle = [];
+        return '<code>' . implode($output) . '</code>';
+    }
 
-                if ($lineNumberColor = $theme->colors['editorLineNumber.foreground'] ?? null) {
-                    $lineNumberStyle[] = "color: $lineNumberColor";
-                }
+    private function buildLine(array $line, int $index): string
+    {
+        $output = [];
 
-                $lineNumberStyle[] = '-webkit-user-select: none';
-                $lineNumberStyle[] = 'user-select: none';
-            
-                $lineNumberStyles = implode('; ', $lineNumberStyle) . ';';
-            
-                $html[] = sprintf(
-                    '<span class="line-number" style="%s">%2d</span>',
-                    $lineNumberStyles,
-                    $i + 1
-                );
-            }  
+        if ($this->withGutter) {
+            $output[] = $this->buildLineNumber($index + 1);
+        }
 
-            foreach ($line as $token) {
-                if ($token->settings === []) {
-                    $html[] = sprintf(
-                        '<span class="token">%s</span>',
-                        htmlspecialchars($token->token->text),
-                    );
+        foreach ($line as $token) {
+            $output[] = $this->buildToken($token);
+        }
 
-                    continue;
-                }
+        return '<span class="line">' . implode($output) . '</span>';
+    }
 
-                $tokenStyles = [
-                    ($token->settings[$defaultThemeId] ?? null)?->toStyleString(),
-                ];
+    private function buildLineNumber(int $lineNumber): string
+    {
+        $lineNumberColor = $this->getDefaultTheme()->colors['editorLineNumber.foreground'] ?? null;
 
-                foreach ($token->settings as $id => $settings) {
-                    if ($id === $defaultThemeId) {
-                        continue;
-                    }
+        $lineNumberStyles = $lineNumberColor ? "color: $lineNumberColor; " : null;
+        $lineNumberStyles .= '-webkit-user-select: none; user-select: none;';
 
-                    $tokenStyles[] = $settings->toCssVarString($id);
-                }
+        return sprintf(
+            '<span class="line-number" style="%s">%2d</span>',
+            $lineNumberStyles,
+            $lineNumber
+        );
+    }
 
-                $html[] = sprintf(
-                    '<span class="token" style="%s">%s</span>',
-                    implode(';', array_filter($tokenStyles)),
-                    htmlspecialchars($token->token->text),
-                );
+    private function buildToken(object $token): string
+    {
+        $tokenStyles = [($token->settings[$this->getDefaultThemeId()] ?? null)?->toStyleString()];
+
+        foreach ($token->settings as $id => $settings) {
+            if ($id !== $this->getDefaultThemeId()) {
+                $tokenStyles[] = $settings->toCssVarString($id);
             }
-
-            $html[] = '</span>';
         }
 
-        $html[] = '</code>';
-        $html[] = '</pre>';
+        $styleString = implode(';', array_filter($tokenStyles));
 
-        if ($this->withWrapper) {
-            $html[] = '</div>';
-        }
+        return sprintf(
+            '<span class="token"%s>%s</span>',
+            $styleString ? " style=\"$styleString\"" : null,
+            htmlspecialchars($token->token->text)
+        );
+    }
 
-        return implode('', $html);
+    private function getDefaultTheme(): ParsedTheme
+    {
+        return Arr::first($this->themes);
+    }
+
+    private function getDefaultThemeId(): string
+    {
+        return Arr::firstKey($this->themes);
     }
 }
