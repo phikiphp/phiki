85957e4474f49232e3ab8ff757a09ca23e2648b6
Ryan Chandler <ryangjchandler@gmail.com>
2025-04-12 20:09:13 +0100

    Proof of concept compliance/comparison helpers

---
+++ tests/Fixtures/vscode-textmate-compliance.js
@@ -0,0 +1,67 @@
+const process = require('node:process');
+const fs = require('node:fs');
+
+const sample = process.argv[2];
+const grammar = process.argv[3];
+const scopeMap = JSON.parse(process.argv[4]);
+
+function readFile(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (error, data) =>
+            error ? reject(error) : resolve(data)
+        );
+    });
+}
+
+const wasmBin = fs.readFileSync(
+    path.join(__dirname, "../../node_modules/vscode-oniguruma/release/onig.wasm")
+).buffer;
+
+const vscodeOnigurumaLib = oniguruma.loadWASM(wasmBin).then(() => {
+    return {
+        createOnigScanner(patterns) {
+            return new oniguruma.OnigScanner(patterns);
+        },
+        createOnigString(s) {
+            return new oniguruma.OnigString(s);
+        },
+    };
+});
+
+const registry = new vsctm.Registry({
+    onigLib: vscodeOnigurumaLib,
+    loadGrammar: (scopeName) => {
+        if (! scopeMap[scopeName]) {
+            console.error(`Unknown scope name: ${scopeName}`);
+            process.exit(1);
+        }
+
+        return readFile(scopeMap[scopeName]).then(data => {
+            return vsctm.parseRawGrammar(data.toString(), scopeMap[scopeName]);
+        })
+    },
+});
+
+const tokens = []
+
+registry.loadGrammar(grammar).then(async grammar => {
+    const text = await readFile(sample);
+    const lines = text.toString().split(/\r?\n/);
+
+    let ruleStack = vsctm.INITIAL;
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        const lineTokens = grammar.tokenizeLine(line, ruleStack);
+
+        tokens.push(
+            lineTokens.map(lineToken => ({
+                scopes: lineToken.scopes,
+                text: line.substring(lineToken.startIndex, lineToken.endIndex),
+                start: lineToken.startIndex,
+                end: lineToken.endIndex,
+            }))
+        )
+
+        ruleStack = lineTokens.ruleStack;
+    }
+})
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,8 +1,11 @@
 <?php
 
 use Phiki\Environment\Environment;
+use Phiki\Grammar\DefaultGrammars;
 use Phiki\Grammar\ParsedGrammar;
+use Phiki\Token\Token;
 use Phiki\Tokenizer;
+use Symfony\Component\Process\Process;
 
 function tokenize(string $input, array $grammar): array
 {
@@ -12,4 +15,40 @@ function tokenize(string $input, array $grammar): array
     );
 
     return $tokenizer->tokenize($input);
+}
+
+function vscodeTextmateTokenize(string $samplePath, string $grammarName): array
+{
+    $process = new Process(
+        [
+            'node',
+            __DIR__ . '/Fixtures/vscode-textmate-compliance.js',
+            $samplePath,
+            $grammarName,
+            json_encode(collect(DefaultGrammars::SCOPES_TO_NAMES)
+                ->mapWithKeys(fn (string $name, string $scope) => [$scope => DefaultGrammars::NAMES_TO_PATHS[$name]])
+                ->all()),
+        ],
+    );
+
+    $process->run();
+
+    if (! $process->isSuccessful()) {
+        throw new RuntimeException($process->getErrorOutput());
+    }
+
+    $output = json_decode($process->getOutput(), true);
+
+    if (! is_array($output)) {
+        throw new RuntimeException('Invalid output from process');
+    }
+
+    return collect($output)
+        ->map(fn (array $token) => new Token(
+            scopes: $token['scopes'],
+            text: $token['text'],
+            start: $token['start'],
+            end: $token['end'],
+        ))
+        ->all();
 }
