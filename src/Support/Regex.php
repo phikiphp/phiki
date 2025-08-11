de764ceb2261d4143dc10df89f84401b933b0d4c
Ryan Chandler <ryangjchandler@gmail.com>
2025-08-11 19:19:57 +0100

    Remove PCRE-translation steps from Regex class

--- src/Support/Regex.php
+++ src/Support/Regex.php
@@ -4,22 +4,8 @@ namespace Phiki\Support;
 
 use Stringable;
 
-/**
- * This class is responsible for converting an Oniguruma pattern into a PCRE2/PHP compatible pattern.
- */
 class Regex implements Stringable
 {
-    const SLASH_P_MAP = [
-        'number' => '0-9',
-        'alnum' => '0-9A-Za-z',
-        'alpha' => 'A-Za-z',
-        'alphabetic' => 'A-Za-z',
-        'blank' => '\\s',
-        'greek' => '\\p{Greek}',
-        'print' => '\\p{L}\\p{N}\\p{P}\\p{S}\\p{Zs}',
-        'word' => '\\w',
-    ];
-
     protected bool $hasAnchor;
 
     protected array $anchorCache = [];
@@ -65,13 +51,6 @@ class Regex implements Stringable
             $this->pattern = implode('', $output);
         }
 
-        $this->pattern = preg_replace('/(?<!\\\)\//', '\\/', $this->pattern);
-        $this->pattern = $this->convertEscapeSequences($this->pattern);
-        $this->pattern = $this->convertUnicodeProperties($this->pattern);
-        $this->pattern = $this->escapeInvalidLeadingRangeCharacter($this->pattern);
-        $this->pattern = $this->escapeUnescapedCloseSetCharacters($this->pattern);
-        $this->pattern = $this->convertUnsupportedUnicodeEscapes($this->pattern);
-
         if ($this->hasAnchor) {
             $this->anchorCache = $this->buildAnchorCache();
         }
@@ -80,68 +59,6 @@ class Regex implements Stringable
     public function get(bool $allowA = false, bool $allowG = false): string
     {
         return $this->resolveAnchors($this->pattern, $allowA, $allowG);
-    }
-
-    protected function convertEscapeSequences(string $pattern): string
-    {
-        // Convert \h to [0-9A-Fa-f].
-        $pattern = preg_replace('/\\\\h/', '[0-9A-Fa-f]', $pattern);
-
-        // Convert \H to [^0-9A-Fa-f].
-        $pattern = preg_replace('/\\\\H/', '[^0-9A-Fa-f]', $pattern);
-
-        // Remove \R as it is not supported in PCRE.
-        $pattern = preg_replace('/\\\\R/', '', $pattern);
-
-        // Remove dangling \p without braces.
-        $pattern = preg_replace('/\\\\p(?![{])/', '', $pattern);
-
-        return $pattern;
-    }
-
-    protected function convertUnicodeProperties(string $pattern): string
-    {
-        // Convert \p{xx} to PCRE-compatible \p{xx}.
-        $pattern = preg_replace_callback('/\\\p\{([a-zA-Z]+)\}/', function (array $matches) {
-            $property = strtolower($matches[1]);
-
-            if (isset(self::SLASH_P_MAP[$property])) {
-                return '['.self::SLASH_P_MAP[$property].']';
-            }
-
-            return $matches[0];
-        }, $pattern);
-
-        return $pattern;
-    }
-
-    protected function escapeInvalidLeadingRangeCharacter(string $pattern): string
-    {
-        // Escape invalid leading range function characters, e.g. [-...].
-        $pattern = preg_replace('/\[(?<!\\\)(-)/', '[\\-', $pattern);
-
-        return $pattern;
-    }
-
-    protected function escapeUnescapedCloseSetCharacters(string $pattern): string
-    {
-        // Escape unescaped close set characters, e.g. ]] converted to \]].
-        $pattern = preg_replace('/(?<!:)(?<!\\\)\]\]/', '\\]]', $pattern);
-
-        return $pattern;
-    }
-
-    protected function convertUnsupportedUnicodeEscapes(string $pattern): string
-    {
-        // Convert \uXXXX to \x{XXXX}.
-        $pattern = preg_replace('/\\\\u([0-9A-Fa-f]{4})/', '\\x{$1}', $pattern);
-
-        // Convert 5+ digit \x{AXXXX} to 4 digit \x{XXXX}.
-        $pattern = preg_replace_callback('/\\\\x\{([0-9A-Fa-f]{5,})\}/', function (array $matches) {
-            return '\\x{'.substr($matches[1], -4).'}';
-        }, $pattern);
-
-        return $pattern;
     }
 
     private function buildAnchorCache(): array
@@ -201,19 +118,12 @@ class Regex implements Stringable
             return $pattern;
         }
 
-        if ($allowA) {
-            if ($allowG) {
-                return $this->anchorCache['A1_G1'];
-            } else {
-                return $this->anchorCache['A1_G0'];
-            }
-        }
-
-        if ($allowG) {
-            return $this->anchorCache['A0_G1'];
-        }
-
-        return $this->anchorCache['A0_G0'];
+        return match (true) {
+            $allowA && $allowG => $this->anchorCache['A1_G1'],
+            $allowA => $this->anchorCache['A1_G0'],
+            $allowG => $this->anchorCache['A0_G1'],
+            default => $this->anchorCache['A0_G0'],
+        };
     }
 
     public function __toString(): string
