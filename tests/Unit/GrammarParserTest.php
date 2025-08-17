1e4ad7a0ccacedc8faf5aa4b241fcce5c6d16ceb
Ryan Chandler <ryangjchandler@gmail.com>
2025-08-17 20:35:04 +0100

    Fix tests

--- tests/Unit/GrammarParserTest.php
+++ tests/Unit/GrammarParserTest.php
@@ -9,7 +9,7 @@ describe('GrammarParser', function () {
 
     it('can parse a grammar file', function () {
         $parser = new GrammarParser;
-        $grammar = $parser->parse(json_decode(file_get_contents(__DIR__.'/../../resources/languages/php.json'), true));
+        $grammar = $parser->parse(json_decode(file_get_contents(__DIR__.'/../../resources/grammars/php.json'), true));
 
         expect($grammar->scopeName)->toBe('source.php');
         expect($grammar->patterns)->toBeArray();
@@ -18,7 +18,7 @@ describe('GrammarParser', function () {
 
     it('can parse a grammar file with injections', function () {
         $parser = new GrammarParser;
-        $grammar = $parser->parse(json_decode(file_get_contents(__DIR__.'/../../resources/languages/blade.json'), true));
+        $grammar = $parser->parse(json_decode(file_get_contents(__DIR__.'/../../resources/grammars/blade.json'), true));
 
         expect($grammar->scopeName)->toBe('text.html.php.blade');
         expect($grammar->getInjections())->toBeArray();
@@ -27,7 +27,7 @@ describe('GrammarParser', function () {
 
     it('marks injection patterns as injected ones', function () {
         $parser = new GrammarParser;
-        $grammar = $parser->parse(json_decode(file_get_contents(__DIR__.'/../../resources/languages/blade.json'), true));
+        $grammar = $parser->parse(json_decode(file_get_contents(__DIR__.'/../../resources/grammars/blade.json'), true));
 
         expect($grammar->scopeName)->toBe('text.html.php.blade');
         expect($grammar->getInjections()[0]->pattern->injection)->toBeTrue();
