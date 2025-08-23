a8f744a11f15924f12433bef71cd96683019a5e4
Ryan Chandler <ryangjchandler@gmail.com>
2025-08-23 17:52:38 +0100

    Simplify comments

--- src/Contracts/GrammarRepositoryInterface.php
+++ src/Contracts/GrammarRepositoryInterface.php
@@ -10,10 +10,6 @@ interface GrammarRepositoryInterface
     /**
      * Get a grammar from the repository.
      *
-     * If the grammar is not already loaded, it will be loaded and cached.
-     *
-     * @param  string  $name  The name of the grammar.
-     *
      * @throws \Phiki\Exceptions\UnrecognisedGrammarException If the grammar is not registered.
      */
     public function get(string $name): ParsedGrammar;
@@ -21,24 +17,17 @@ interface GrammarRepositoryInterface
     /**
      * Get a grammar from the repository by scope name.
      *
-     * @param  string  $scope  The name of the scope.
-     *
      * @throws \Phiki\Exceptions\UnrecognisedGrammarException If the grammar is not registered.
      */
     public function getFromScope(string $scope): ParsedGrammar;
 
     /**
      * Check whether a grammar exists in the repository.
-     *
-     * @param  string  $name  The name of the grammar.
      */
     public function has(string $name): bool;
 
     /**
-     * Register a new Grammar to use when highlighting.
-     *
-     * @param  string  $name  The name of the grammar.
-     * @param  string|ParsedGrammar  $pathOrGrammar  The path to the grammar file or the grammar itself.
+     * Register a new grammar to use when highlighting.
      */
     public function register(string $name, string|ParsedGrammar $pathOrGrammar): void;
 
--- src/Contracts/ThemeRepositoryInterface.php
+++ src/Contracts/ThemeRepositoryInterface.php
@@ -12,24 +12,17 @@ interface ThemeRepositoryInterface
      *
      * If the theme is not already loaded, it will be loaded and cached.
      *
-     * @param  string  $name  The name of the theme.
-     *
      * @throws \Phiki\Exceptions\UnrecognisedThemeException If the theme is not registered.
      */
     public function get(string $name): ParsedTheme;
 
     /**
      * Check whether a theme exists in the repository.
-     *
-     * @param  string  $name  The name of the theme.
      */
     public function has(string $name): bool;
 
     /**
      * Register a new theme to use when highlighting.
-     *
-     * @param  string  $name  The name of the theme.
-     * @param  string|ParsedTheme  $pathOrTheme  The path to the theme file or the theme itself.
      */
     public function register(string $name, string|ParsedTheme $pathOrTheme): void;
 
